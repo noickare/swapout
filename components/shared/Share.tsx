37eea43758d55bc22e0e03d5fbc3b604d357277f
noickare <likono@clueconn.com>
2022-06-23 14:44:53 -0500

    share-url

--- components/shared/Share.tsx
+++ components/shared/Share.tsx
@@ -19,93 +19,96 @@ import { Button, Dropdown, Menu, Space } from 'antd';
 import { ShareAltOutlined, SmileOutlined } from '@ant-design/icons';
 
 
-type Props = {}
-
-const menu = (
-  <Menu
-    style={{ display: 'flex' }}
-    items={
-      [
-        {
-          key: '1',
-          label: (
-            <TwitterShareButton
-              url={'https://github.com/next-share'}
-              title={'next-share is a social share buttons for your next React apps.'}
-            >
-              <TwitterIcon size={32} round />
-            </TwitterShareButton>
-          ),
-        },
-        {
-          key: '2',
-          label: (
-            <FacebookShareButton
-              url={'http://localhost:3000'} >
-              <FacebookIcon size={32} round />
-            </FacebookShareButton>
-          ),
-        },
-        {
-          key: '3',
-          label: (
-            <PinterestShareButton
-              url={'http://localhost:3000'}
-              media={'next-share is a social share buttons for your next React apps.'}
-            >
-              <PinterestIcon size={32} round />
-            </PinterestShareButton>
-          ),
-        },
-        {
-          key: '4',
-          danger: true,
-          label: (
-            <RedditShareButton
-              url={'http://localhost:3000'} >
-              <RedditIcon size={32} round />
-            </RedditShareButton>
-          ),
-        },
-        {
-          key: '5',
-          danger: true,
-          label: (
-            <WhatsappShareButton
-              url={'http://localhost:3000'} >
-              <WhatsappIcon size={32} round />
-            </WhatsappShareButton>
-          ),
-        },
-        {
-          key: '6',
-          danger: true,
-          label: (
-            <LinkedinShareButton
-              url={'http://localhost:3000'} >
-              <LinkedinIcon size={32} round />
-            </LinkedinShareButton>
-          ),
-        },
-        {
-          key: '7',
-          danger: true,
-          label: (
-            <EmailShareButton
-              url={'https://github.com/next-share'}
-              subject={'Next Share'}
-              body="body"
-            >
-              <EmailIcon size={32} round />
-            </EmailShareButton>
-          ),
-        },
-      ]}
-  />
-);
+type Props = {
+  url: string;
+  title: string;
+}
 
 
-export default function Share({ }: Props) {
+
+export default function Share({ url, title }: Props) {
+  const menu = (
+    <Menu
+      style={{ display: 'flex' }}
+      items={
+        [
+          {
+            key: '1',
+            label: (
+              <TwitterShareButton
+                url={url}
+                title={title}
+              >
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+            ),
+          },
+          {
+            key: '2',
+            label: (
+              <FacebookShareButton
+                url={url} >
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+            ),
+          },
+          {
+            key: '3',
+            label: (
+              <PinterestShareButton
+                url={url}
+                media={title}
+              >
+                <PinterestIcon size={32} round />
+              </PinterestShareButton>
+            ),
+          },
+          {
+            key: '4',
+            danger: true,
+            label: (
+              <RedditShareButton
+                url={url} >
+                <RedditIcon size={32} round />
+              </RedditShareButton>
+            ),
+          },
+          {
+            key: '5',
+            danger: true,
+            label: (
+              <WhatsappShareButton
+                url={url} >
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+            ),
+          },
+          {
+            key: '6',
+            danger: true,
+            label: (
+              <LinkedinShareButton
+                url={url} >
+                <LinkedinIcon size={32} round />
+              </LinkedinShareButton>
+            ),
+          },
+          {
+            key: '7',
+            danger: true,
+            label: (
+              <EmailShareButton
+                url={url}
+                subject={'Next Share'}
+                body="body"
+              >
+                <EmailIcon size={32} round />
+              </EmailShareButton>
+            ),
+          },
+        ]}
+    />
+  );
   return (
     <div>
       <div>
