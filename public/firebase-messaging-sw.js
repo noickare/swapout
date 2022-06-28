e5148c713b24fdf5dafab18aacddb282e7cedbe0
noickare <likono@clueconn.com>
2022-06-28 12:04:05 -0500

    notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,38 +1,27 @@
-importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js"
+);
 
-firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-});
+// Initialize the Firebase app in the service worker by passing the generated config
+var firebaseConfig = {
+  messagingSenderId: "151596128119",
+};
 
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
 const messaging = firebase.messaging();
 
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
 
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload)
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png'
-  }
+  };
 
-  return self.registration.showNotification(notificationTitle, notificationOptions)
-})
-
-messaging.onBackgroundMessage(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload)
-  const notificationTitle = payload.notification.title
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/firebase-logo.png'
-  }
-
-  return self.registration.showNotification(notificationTitle, notificationOptions)
-})
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
